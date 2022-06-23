3fd92e77e431d06478b92a8140a56961da6a256e
stilin <107877104+stilin@users.noreply.github.com>
2022-06-23 13:56:33 +0200

    Create BSL_P5.rkt

---
+++ BSL_P5.rkt
@@ -0,0 +1,26 @@
+(require 2htdp/image)
+
+;; compare-images-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; Based on the two constants provided, write three expressions to determine whether: 
+; 
+; 1) IMAGE1 is taller than IMAGE2
+; 2) IMAGE1 is narrower than IMAGE2
+; 3) IMAGE1 has both the same width AND height as IMAGE2
+; 
+
+
+(define IMAGE1 (rectangle 10 15 "solid" "red"))
+(define IMAGE2 (rectangle 15 10 "solid" "red"))
+
+(> (image-height IMAGE1) (image-height IMAGE2))
+; IMAGE1 is taller
+(< (image-width IMAGE1) (image-width IMAGE2))
+; IMAGE1 is narrower
+
+(and (= (image-height IMAGE1) (image-height IMAGE2))
+     (= (image-width IMAGE1) (image-width IMAGE2)))
+; IMAGE1 does not have the same width/height as IMAGE2
